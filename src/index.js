d2786aed99e35d75cca360023aab4d351c6d6d68
wabemusic <jvwatanabe@gmail.com>
2025-07-06 18:18:24 -0300

    add: main function and race engine

--- src/index.js
+++ src/index.js
@@ -15,10 +15,32 @@ const player2 = {
   PONTOS: 0,
 };
 
-// Para a batalha da pista, precisamos criar a lógica de dados - Roll Dice
+// Para a batalha da pista, precisamos criar a logica de dados - Roll Dice
 
 const rollDice = async () => {
-  // ramdom tira 0 ou 1 e para dar 6 multiplicamos por, logo preciamos arredondar o valor, que irá de 0 a 5.algumacoisa logo preciamos somar 1.
+  // ramdom tira 0 ou 1 e para dar 6 multiplicamos por, logo preciamos arredondar o valor, que ira de 0 a 5.algumacoisa logo preciamos somar 1.
   return Math.floor(Math.random() * 6) + 1;
 };
 
+
+// Motor que faz o jogo funcionar
+const playRaceEngine = async (character1, character2) => {
+  for(let i = 1; i <= 5; i++) {
+    console.log(`🏁 Rodada ${i} 🏁`);
+
+    // Sortear o bloco
+    
+  }
+};
+
+// Cada corrida consiste em 5 rodadas de um bloco de pistas aleatorios (reta, curva ou confornto)
+
+// A funcão main vai chamar as outras funcoes, sendo ela a principal/ funcao de entrada
+// para deixar uma funcao auto invocavel usamos (funcao)()
+(async () => {
+  console.log(
+    `🏁 Corrida entre ${player1.NOME} VS ${player2.NOME} começando... \n`
+  );
+
+  await playRaceEngine(player1, player2)
+})();
