46b4ccd76447571f294b562aed6fb29421f68938
wabemusic <jvwatanabe@gmail.com>
2025-07-14 22:45:17 -0300

    fix: refact if's and confronto message

--- src/index.js
+++ src/index.js
@@ -123,11 +123,16 @@ const playRaceEngine = async (character1, character2) => {
         character2.PODER
       );
 
-      character2.PONTOS -=
-        powerResult1 > powerResult2 && character2.PONTOS > 0 ? 1 : 0;
+      if (powerResult1 > powerResult2 && character2.PONTOS > 0) {
+        console.log(`${character1.NOME} venceu o confronto! ${character2.NOME} perdeu 1 ponto!`);
+        
+        character2.PONTOS--;
+      }
 
-      character1.PONTOS -=
-        powerResult2 > powerResult1 && character1.PONTOS > 0 ? 1 : 0;
+      if (powerResult2 > powerResult1 && character1.PONTOS > 0) {
+        console.log(`${character2.NOME} venceu o confronto! ${character1.NOME} perdeu 1 ponto!`);
+        character1.PONTOS--;
+      }
 
       console.log(
         powerResult1 === powerResult2
